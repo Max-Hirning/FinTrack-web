6bc4615ab77c1bffbd7dfca2f4b19a40c411ff79
Max-Hirning <maxhirning25@gmail.com>
2024-03-08 15:33:34 +0200

    Added husky and eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -17,4 +17,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
