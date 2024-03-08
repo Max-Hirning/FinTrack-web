d9d29376a6d52b5ec16104f9799e2a3562eaa8eb
Max-Hirning <maxhirning25@gmail.com>
2024-03-08 17:08:10 +0200

    Added custom styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,17 @@ import React from "react";
 
 export default function Home() {
   return (
-    <h1>MAIN PAGE</h1>
+    <main>
+      <section className="card w-[730px] h-[322px] p-[20px]">
+        <input className="input" placeholder="Hello world"/>
+        <h1 className="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore accusamus veritatis suscipit voluptas pariatur labore quidem eveniet dolorum, laudantium maiores ipsam exercitationem possimus dignissimos. Pariatur unde repellendus officiis quae vel.</h1>
+      </section>
+      <section className="card w-[730px] h-[322px] p-[20px]">
+        <h1 className="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore accusamus veritatis suscipit voluptas pariatur labore quidem eveniet dolorum, laudantium maiores ipsam exercitationem possimus dignissimos. Pariatur unde repellendus officiis quae vel.</h1>
+      </section>
+      <button className="button w-[190px] h-[50px]">Save</button>
+      <button className="text-button w-[190px] h-[50px]">Save</button>
+      <button className="outlined-button w-[190px] h-[50px]">Save</button>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,81 @@
 import type {Config} from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        "sm": "376px",
+        "md": "426px",
+        "lg": "851px",
+        "xl": "1025px",
+        "2xl": "1441px",
+      },
+      colors: {
+        "text": "#343C6A",
+        "main": "#2D60FF",
+        "danger": "#FF4B4A",
+        "success": "#41D4A8",
+        "secondary": "718EBF",
+        "disabled": "#B1B1B1",
       },
     },
   },
-  plugins: [],
+  content: [
+    "./src/UI/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [
+    plugin(function({addUtilities, addComponents}) {
+      addComponents({
+        ".card": {
+          borderRadius: "25px",
+          backgroundColor: "white",
+          boxShadow: "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)"
+        },
+        ".input": {
+          padding: "13px",
+          fontSize: "15px",
+          color: "#718EBF",
+          borderRadius: "15px",
+          border: "1px solid #DFEAF2"
+        },
+        ".button": {
+          color: "white",
+          display: "flex",
+          borderRadius: "15px",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#1814F3",
+        },
+        ".text-button": {
+          display: "flex",
+          color: "#1814F3",
+          borderRadius: "15px",
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        ".outlined-button": {
+          display: "flex",
+          color: "#1814F3",
+          borderRadius: "15px",
+          alignItems: "center",
+          justifyContent: "center",
+          border: "2px solid #1814F3"
+        }
+      });
+      addUtilities({
+        ".title": {
+          overflow: "hidden",
+          whiteSpace: "nowrap",
+          textOverflow: "ellipsis",
+        },
+      });
+    }),
+  ],
+  important: true,
 };
 
 export default config;
