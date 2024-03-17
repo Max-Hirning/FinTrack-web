ff4d0c578d4c7c17c636a8f975308adedb82a5fd
Max-Hirning <maxhirning25@gmail.com>
2024-03-17 13:52:06 +0200

    Renamed files

--- src/modules/analytics/index.ts
+++ src/modules/analytics/index.ts
@@ -1,4 +1,4 @@
 export type {IAccountResponse} from "./types/account";
 export type {ICardsExpensesResponse} from "./types/cardsExpenses";
-export {AccountsWrappers} from "./view/wrappers/accountsWrappers";
-export {CardExpenseStatisticsWrappers} from "./view/wrappers/cardExpenseStatisticsWrappers";
+export {AccountsWrappers} from "./view/wrappers/accountsWrappers1";
+export {CardExpenseStatisticsWrappers} from "./view/wrappers/cardExpenseStatisticsWrappers1";
--- src/modules/analytics/view/wrappers/cardExpenseStatisticsWrappers1.tsx
+++ src/modules/analytics/view/wrappers/cardExpenseStatisticsWrappers1.tsx
@@ -1,33 +1,32 @@
 import React from "react";
-import {Accounts} from "../accounts";
 import {getServerSession} from "next-auth";
 import {QueryKeys} from "@/configs/queryKeys";
 import {IUserSession} from "@/modules/profile";
 import {authOptions} from "@/configs/authOptions";
 import {analyticsAPI} from "../../controllers/api";
-import {IAccountFilters} from "../../types/account";
 import {getStartEndOfMonth} from "@/controllers/dates";
+import {CardExpenseStatistics} from "../cardExpenseStatistics1";
+import {ICardsExpensesFilters} from "../../types/cardsExpenses";
 import {HydrationBoundary, QueryClient, dehydrate} from "@tanstack/react-query";
 
-export async function AccountsWrappers() {
+export async function CardExpenseStatisticsWrappers() {
   const queryClient = new QueryClient();
   const session = await getServerSession(authOptions);
 
-  const filters: IAccountFilters = {
-    currency: (session?.user as IUserSession).currency, 
-    cards: {cards: (session?.user as IUserSession).cards}, 
-    transactions: {cards: (session?.user as IUserSession).cards, date: getStartEndOfMonth()},
+  const filters: ICardsExpensesFilters = {
+    currency: (session?.user as IUserSession).currency,
+    filters: {cards: (session?.user as IUserSession).cards, date: getStartEndOfMonth()}
   };
 
   await queryClient.prefetchQuery({
     // eslint-disable-next-line @tanstack/query/exhaustive-deps
-    queryKey: [QueryKeys.getInfo, JSON.stringify(filters)],
-    queryFn: () => analyticsAPI.getAccountInfo(filters, (session?.user as IUserSession).jwt)
+    queryKey: [QueryKeys.getCardsExpenses, JSON.stringify(filters)],
+    queryFn: () => analyticsAPI.getCardsExpenses(filters, (session?.user as IUserSession).jwt)
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Accounts
+      <CardExpenseStatistics
         filters={filters}
         session={session?.user as IUserSession}
       />
