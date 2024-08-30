5c953d60c5d818a4bfff9c1fd4296e7a89fa02e5
Max-Hirning <maxhirning25@gmail.com>
2024-08-30 21:11:03 +0300

    fix: undo eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Checkbox } from "shared/ui/checkbox";
 import { Button } from "shared/ui/button";
 
 export default function Home() {
   return (
     <main>
       <Button>Click me</Button>
+      <Checkbox />
     </main>
   );
 }
