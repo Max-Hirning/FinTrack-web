59eff369c6ffdb9baa12f2264a6924b84ac4aa3e
Max-Hirning <maxhirning25@gmail.com>
2024-09-08 18:20:16 +0300

    feat: added menu for cards

--- src/shared/ui/bank-card.tsx
+++ src/shared/ui/bank-card.tsx
@@ -1,20 +1,28 @@
 import { Cpu } from "lucide-react";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "shared/ui";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle, ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "shared/ui";
 
 export function BankCard() {
   return (
-    <Card className="w-full max-w-[350px] min-w-[350px] h-[235px] justify-between flex flex-col">
-      <CardHeader className="flex flex-row items-center justify-between">
-        <article>
-          <CardTitle className="text-lg font-semibold">John Doe</CardTitle>
-          <CardTitle className="text-lg font-semibold">**9793</CardTitle>
-        </article>
-        <Cpu />
-      </CardHeader>
-      <CardFooter className="flex-col items-start">
-        <CardTitle className="font-normal text-base">Balance</CardTitle>
-        <CardDescription className="font-semibold text-2xl">USD 41900.01</CardDescription>
-      </CardFooter>
-    </Card>
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <Card className="w-full max-w-[350px] min-w-[350px] h-[235px] justify-between flex flex-col">
+          <CardHeader className="flex flex-row items-center justify-between">
+            <article>
+              <CardTitle className="text-lg font-semibold">John Doe</CardTitle>
+              <CardTitle className="text-lg font-semibold">**9793</CardTitle>
+            </article>
+            <Cpu />
+          </CardHeader>
+          <CardFooter className="flex-col items-start">
+            <CardTitle className="font-normal text-base">Balance</CardTitle>
+            <CardDescription className="font-semibold text-2xl">USD 41900.01</CardDescription>
+          </CardFooter>
+        </Card>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem>Edit</ContextMenuItem>
+        <ContextMenuItem>Delete</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   )
 }
